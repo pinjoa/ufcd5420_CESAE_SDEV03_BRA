66f27018a17c9c13cce68ff10140b79c28800c09
João Pinto <6815379+pinjoa@users.noreply.github.com>
2023-06-05 16:53:27 +0100

    Contador Unico

---
+++ Task02/Conceitos/ContadorUnico/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace ContadorUnico
+{
+    public class Contador
+    {
+        private static Contador instancia;
+        private static int contador = 0;
+    
+        private Contador()
+        {
+        }
+    
+        public static Contador Instancia
+        {
+            get
+            {
+                if (instancia == null)
+                {
+                    instancia = new Contador();
+                }
+                return instancia;
+            }
+        }
+
+        public int Proximo()
+        {
+            return ++contador;
+        }
+    
+    }
+    
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(Contador.Instancia.Proximo());
+
+            }
+        }
+    }
+}
